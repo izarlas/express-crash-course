a2fda063845ba37bcc968b6280629a63155ce19b
Ioannis Zarlas <ioannis.zarlas@chromaway.com>
2025-06-07 02:09:39 +0300

    CARDS CRUD

--- src/apis/cards.ts
+++ src/apis/cards.ts
@@ -4,36 +4,72 @@ import { stringSchema } from "../validations/primitiveSchemas";
 
 const router: Router = express.Router();
 
+// Get all cards
 router.get("/", (req: Request, res: Response) => {
   const limitParam = req.query.limit;
 
   if (limitParam === undefined) {
-    return res.status(200).json(MOCK_CARDS);
+    res.status(200).json(MOCK_CARDS);
   }
 
   const stringLimitParam = stringSchema.safeParse(limitParam);
 
-  if (!stringLimitParam.success) {
-    throw new Error(`Limit param ${limitParam} is not a string!`);
+  if (stringLimitParam.success) {
+    const intParsedLimit = parseInt(stringLimitParam.data);
+
+    if (!isNaN(intParsedLimit) && intParsedLimit > 0) {
+      res.status(200).json(MOCK_CARDS.slice(0, intParsedLimit));
+    }
   }
 
-  const intParsedLimit = parseInt(stringLimitParam.data);
-
-  if (!isNaN(intParsedLimit) && intParsedLimit > 0) {
-    return res.status(200).json(MOCK_CARDS.slice(0, intParsedLimit));
-  }
-
-  return res.status(200).json(MOCK_CARDS);
+  res.status(200).json(MOCK_CARDS);
 });
 
+// Get single card (by id)
 router.get("/:id", (req: Request, res: Response) => {
   const id = parseInt(req.params.id);
   const foundCard = MOCK_CARDS.find((card) => card.id === id);
 
   if (!foundCard)
-    return res.status(404).json({ msg: `Card with id ${id} was not found` });
+    res.status(404).json({ msg: `Card with id ${id} was not found` });
 
-  return res.status(200).json(foundCard);
+  res.status(200).json(foundCard);
+});
+
+// Create new card
+router.post("/", (req: Request, res: Response) => {
+  const newCard = { id: MOCK_CARDS.length + 1, title: req.body.title };
+
+  if (!newCard.title) {
+    res.status(400).json({ msg: "Please include a title" });
+  }
+
+  res.status(201).json(MOCK_CARDS);
+});
+
+// Update card (by id)
+router.put("/:id", (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const foundCard = MOCK_CARDS.find((card) => card.id === id);
+
+  if (!foundCard) {
+    res.status(404).json({ msg: `Card with id ${id} was not found` });
+  }
+
+  foundCard!.title = req.body.title;
+  res.status(200).json(MOCK_CARDS);
+});
+
+// Delete card (by id)
+router.delete("/:id", (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const foundCard = MOCK_CARDS.find((card) => card.id === id);
+
+  if (!foundCard) {
+    res.status(404).json({ msg: `Card with id ${id} was not found` });
+  }
+
+  res.status(200).json(MOCK_CARDS.filter(card => card.id !== id))
 });
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,11 @@ import home from "./apis/home";
 const app = express();
 const port = process.env.PORT || serverPort;
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
 app.use("/api/home", home);
 app.use("/api/cards", cards);
 
